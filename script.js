4dbc8c596c5b5956fa4fb702b9931e5a92d54f65
MohmedHussain <163523639+Hussain-Ravla@users.noreply.github.com>
2025-06-17 13:45:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+  let count = 0;
+  for (let i = 5; n / i >= 1; i *= 5) {
+    count += Math.floor(n / i);
+  }
+  return count;
 }
 
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
+if (!isNaN(input) && input >= 0) {
+  alert(`Number of trailing zeros in ${input}! is: ${trailingZeros(input)}`);
+} else {
+  alert("Please enter a valid non-negative integer.");
+}
